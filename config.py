a497389b0bb40ef32f2f553ece80dc5afff52286
nothing <161198246+PYHOMES00@users.noreply.github.com>
2024-07-09 20:43:59 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,38 @@
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
+
+
+
+import os
+
+class Config:
+    API_ID = os.environ.get("API_ID", "28243586")
+    API_HASH = os.environ.get("API_HASH", "4022d5686b9b7a7cf8891205921a0ab3")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6769575529:AAEvaEvd-em2ThrbVum3zfxhhmTPWHLEG-E") 
+    BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://madarazbotz:Dy1i2Ap8RR6AGpGc@cluster0.my7zhtd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "Cluster0")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+
+
+class temp(object): 
+    lock = {}
+    CANCEL = {}
+    forwardings = 0
+    BANNED_USERS = []
+    IS_FRWD_CHAT = []
+    
+
+
+
+
+
+
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
